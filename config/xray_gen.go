3d7ece3762c221512a75226330031898093db948
ashnob3000 <ash.nob3000@atomicmail.io>
2026-09-05 03:28:16 +0330

    Update xray_gen.go

--- config/xray_gen.go
+++ config/xray_gen.go
@@ -3,37 +3,136 @@ package config
 import (
 	"encoding/json"
 	"fmt"
+
 	"github.com/google/uuid"
 )
 
+type Client struct {
+	UUID     string
+	Password string
+}
+
+func GenerateXrayConfig(clients []Client) ([]byte, error) {
+	vlessClients := make([]map[string]interface{}, 0)
+	trojanClients := make([]map[string]interface{}, 0)
+
+	for _, client := range clients {
+		if client.UUID != "" {
+			vlessClients = append(vlessClients, map[string]interface{}{
+				"id": client.UUID,
+			})
+		}
+
+		if client.Password != "" {
+			trojanClients = append(trojanClients, map[string]interface{}{
+				"password": client.Password,
+			})
+		}
+	}
+
+	cfg := map[string]interface{}{
+		"log": map[string]interface{}{
+			"loglevel": "warning",
+		},
+
+		"inbounds": []interface{}{
+			map[string]interface{}{
+				"tag":      "vless-ws",
+				"listen":  "127.0.0.1",
+				"port":    2097,
+				"protocol": "vless",
+
+				"settings": map[string]interface{}{
+					"clients":    vlessClients,
+					"decryption": "none",
+				},
+
+				"streamSettings": map[string]interface{}{
+					"network":  "ws",
+					"security": "none",
+
+					"wsSettings": map[string]interface{}{
+						"path": "/vless",
+					},
+				},
+			},
+
+			map[string]interface{}{
+				"tag":      "trojan-ws",
+				"listen":  "127.0.0.1",
+				"port":    2098,
+				"protocol": "trojan",
+
+				"settings": map[string]interface{}{
+					"clients": trojanClients,
+				},
+
+				"streamSettings": map[string]interface{}{
+					"network":  "ws",
+					"security": "none",
+
+					"wsSettings": map[string]interface{}{
+						"path": "/trojan",
+					},
+				},
+			},
+		},
+
+		"outbounds": []interface{}{
+			map[string]interface{}{
+				"protocol": "freedom",
+				"tag":      "direct",
+			},
+		},
+	}
+
+	return json.MarshalIndent(cfg, "", "  ")
+}
+
 func GenerateVlessConfig(sni, wsHost string) map[string]interface{} {
 	id := uuid.New().String()
+
 	return map[string]interface{}{
 		"protocol": "vless",
 		"settings": map[string]interface{}{
-			"clients": []map[string]string{{"id": id}},
+			"clients": []map[string]string{
+				{"id": id},
+			},
 		},
 		"streamSettings": map[string]interface{}{
 			"network":  "ws",
 			"security": "tls",
-			"wsSettings": map[string]string{"path": "/vless", "host": wsHost},
-			"tlsSettings": map[string]string{"serverName": sni},
+			"wsSettings": map[string]string{
+				"path": "/vless",
+				"host": wsHost,
+			},
+			"tlsSettings": map[string]string{
+				"serverName": sni,
+			},
 		},
 	}
 }
 
 func GenerateTrojanConfig(sni, wsHost string) map[string]interface{} {
 	password := uuid.New().String()
+
 	return map[string]interface{}{
 		"protocol": "trojan",
 		"settings": map[string]interface{}{
-			"clients": []map[string]string{{"password": password}},
+			"clients": []map[string]string{
+				{"password": password},
+			},
 		},
 		"streamSettings": map[string]interface{}{
 			"network":  "ws",
 			"security": "tls",
-			"wsSettings": map[string]string{"path": "/trojan", "host": wsHost},
-			"tlsSettings": map[string]string{"serverName": sni},
+			"wsSettings": map[string]string{
+				"path": "/trojan",
+				"host": wsHost,
+			},
+			"tlsSettings": map[string]string{
+				"serverName": sni,
+			},
 		},
 	}
 }
@@ -43,5 +142,6 @@ func ToJSON(cfg map[string]interface{}) (string, error) {
 	if err != nil {
 		return "", fmt.Errorf("marshal error: %v", err)
 	}
+
 	return string(b), nil
 }
